642fd01959835d9533885ddd464dfd301722d30a
Galvin Santoso <galvinsantoso22@gmail.com>
2021-12-07 23:00:02 +0700

    Push file Lomba

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home bg-primary">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+  <h1 class="font-black m-5">hellow</h1>
+  <button class="bg-btnOrange">Submit</button>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00B3C8 ',
+        btnOrange: '#F36523',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
