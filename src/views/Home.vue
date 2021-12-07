9887365935e87c22e9ecc91d7b855fb2ced24d3f
Galvin Santoso <galvinsantoso22@gmail.com>
2021-12-07 23:05:20 +0700

    change baru

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home bg-primary">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to  Vue.js App" />
   </div>
   <h1 class="font-black m-5">hellow</h1>
   <button class="bg-btnOrange">Submit</button>
+  <h1>helloworld</h1>
 </template>
 
 <script>
